9a14b09289a179cd4a392558f697c5e73bfcf6e0
Dhanuushree <dhanushreedhanu15@gmail.com>
2022-02-26 21:07:23 +0530

    Agile Health Tech

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import NavigationBar from './Component/NavigationBar/NavigationBar';
 import { Grid } from '@mui/material';
-
 function App() {
   return (
     <div className="App">
